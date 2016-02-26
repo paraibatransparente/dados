deebac962b5e0af63248d1e3fca3bd4d2bd2808b
Diego Nobre <dcnobre@gmail.com>
2016-02-26 01:14:50 -0300

    Script de transformação dos arquivos texto em bases de dados sqlite3

---
+++ tce/sagres/esfera-municipal/ddl/estorno.sql
@@ -0,0 +1,10 @@
+CREATE TABLE estorno (
+  cd_UGestora INTEGER,
+  de_ugestora TEXT,
+  dt_Ano INTEGER,
+  de_UOrcamentaria TEXT,
+  nu_Empenho INTEGER,
+  nu_Estorno INTEGER,
+  dt_estorno DATE,
+  vl_Estorno NUMERIC(12,4)
+);
