03b53d4dec99ffd738c29506df3e9c25c8f0f1fd
Diego Nobre <diego@voxtecnologia.com.br>
2016-02-26 21:23:56 -0300

    Atualizando estrutura dos scripts

--- tce/sagres/dados-abertos.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# script de importação de dados
-# Órgão: TCE-PB (Tribunal de Contas do Estado da Paraiba)
-# Origem: http://portal.tce.pb.gov.br/dados-abertos-do-sagres-tcepb/
-
-# download dos dados
-echo "Iniciando download dos dados (Esfera Municipal e Estadual)"
-./dados-abertos-esfera-municipal.sh
-./dados-abertos-esfera-estadual.sh
