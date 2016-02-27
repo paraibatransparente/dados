4b50d797af56e69ee720f362e0d7ec85ede1a39a
Diego Nobre <diego@voxtecnologia.com.br>
2016-02-27 14:53:43 -0300

    Removidos arquivos shell que tiveram regra migrada para python

--- tomando-a-pilula-vermelha.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# script de importação de dados
-# Órgão: TCE-PB (Tribunal de Contas do Estado da Paraiba)
-# Origem: http://portal.tce.pb.gov.br/dados-abertos-do-sagres-tcepb/
-
-# download dos dados
-echo "Iniciando download dos dados (Esfera Municipal e Estadual)"
-tce/sagres/dados-abertos-esfera-municipal.sh
-tce/sagres/dados-abertos-esfera-estadual.sh
