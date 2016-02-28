8c6e46f5ace5850bc63c4d9137ea5ecd985412f6
Diego Nobre <diego@voxtecnologia.com.br>
2016-02-28 20:19:36 -0300

    Script normalizador executa scripts de indice e criação do campo municipio

--- tce/sagres/esfera-municipal/normalizador.py
+++ tce/sagres/esfera-municipal/normalizador.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import sqlite3 # biblioteca necessária para trabalhar com sqlite3
 import os
+from unicodedata import normalize
 
 """
 Script responsável pela normalização das tabelas da ESFERA MUNICIPAL
@@ -16,6 +17,21 @@ def conectar(path_banco):
     conexao.text_factory = str
 
     return conexao
+
+'''
+Devolve cópia de uma str substituindo os caracteres
+acentuados pelos seus equivalentes não acentuados.
+
+ATENÇÃO: carateres gráficos não ASCII e não alfa-numéricos,
+tais como bullets, travessões, aspas assimétricas, etc.
+são simplesmente removidos!
+
+    >>> remover_acentos('[ACENTUAÇÃO] ç: áàãâä! éèêë? íì&#297;îï, óòõôö; úù&#361;ûü.')
+    '[ACENTUACAO] c: aaaaa! eeee? iiiii, ooooo; uuuuu.'
+
+'''
+def remover_acentos(txt, codif='utf-8'):
+    return normalize('NFKD', txt.decode(codif)).encode('ASCII','ignore')
 
 print "# conectando ao banco de dados"
 try:
@@ -34,9 +50,9 @@ for unidade in (cursor.execute('SELECT DISTINCT cd_ugestora, de_ugestora FROM em
     #print str(unidade[0]) + ";" + unidade[1]
     cursor_insert.executemany('INSERT INTO unidade_gestora VALUES (NULL, ?, ?)', (unidade, ))
 
+conexao.commit()
 cursor.close()
 cursor_insert.close()
-conexao.commit()
 
 print "# criando estrutura da tabela CREDOR"
 cursor = conexao.cursor()
@@ -48,8 +64,35 @@ for credor in (cursor.execute('SELECT DISTINCT cd_credor, no_credor FROM empenho
     #print str(credor[0]) + ";" + credor[1]
     cursor_insert.executemany('INSERT INTO credor VALUES (NULL, ?, ?)', (credor, ))
 
+conexao.commit()
 cursor.close()
 cursor_insert.close()
-conexao.commit()
 
+print "Criando tabela de municipios"
+cursor = conexao.cursor()
+cursor.executescript(open(os.getcwd()+'/ddl/municipio.sql').read())
+cursor.close()
+
+print "Alimentando campo ds_link da tabela municipio"
+cursor = conexao.cursor()
+cursor_upd = conexao.cursor()
+for municipio in (cursor.execute('SELECT cd_municipio, de_municipio FROM municipio')):
+    print remover_acentos(municipio[1]).lower().replace(" ", "-")
+    cursor_upd.execute('UPDATE municipio SET ds_link = ? WHERE cd_municipio = ?', (remover_acentos(municipio[1]).lower().replace(" ", "-"), municipio[0], ))
+
+conexao.commit()
+cursor_upd.close()
+cursor.close()
+
+print "# script SQL normalizador"
+cursor = conexao.cursor()
+cursor.executescript(open(os.getcwd()+'/ddl/normalizador.sql').read())
+cursor.close()
+
+print "Criando INDICES"
+cursor = conexao.cursor()
+cursor.executescript(open(os.getcwd()+'/ddl/indices.sql').read())
+cursor.close()
+
+print "Fechando conexão ao banco"
 conexao.close()
