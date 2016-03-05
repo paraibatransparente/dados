f78be35551d47f8d1ab5f000bd5dfa8bbff160b1
Diego Nobre <diego@voxtecnologia.com.br>
2016-03-05 16:40:37 -0300

    Novo campo ds_link

--- tce/sagres/esfera-municipal/ddl/unidade_gestora.sql
+++ tce/sagres/esfera-municipal/ddl/unidade_gestora.sql
@@ -2,5 +2,6 @@ DROP TABLE IF EXISTS unidade_gestora;
 CREATE TABLE unidade_gestora (
   id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
   cd_ugestora INTEGER,
-  de_ugestora TEXT
+  de_ugestora TEXT,
+  ds_link TEXT
 );
