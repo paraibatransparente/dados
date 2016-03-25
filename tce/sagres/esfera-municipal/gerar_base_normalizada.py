0ffbccb6be09f398067f4381fedfe9ab85c35dbe
Diego Nobre <diego@voxtecnologia.com.br>
2016-03-25 08:58:35 -0300

    Script gera comando de dump da base normalizada

---
+++ tce/sagres/esfera-municipal/gerar_base_normalizada.py
@@ -0,0 +1,17 @@
+from subprocess import call
+
+tabelas = [
+    'funcao',
+    'municipio',
+    'tipo_unidade_gestora',
+    'unidade_gestora',
+    'pagamento_historico_gestora_ano',
+    'pagamento_historico_gestora_elemento_ano',
+    'pagamento_historico_gestora_funcao_ano'
+    ]
+
+for tabela in tabelas:
+    cmd_dump = "\".dump '"+tabela+"'\""
+    comando = "sqlite3 esfera-municipal.db " + cmd_dump + " >> esfera-municipal-normalizada.db.sql"
+    print comando
+    #call([comando])
