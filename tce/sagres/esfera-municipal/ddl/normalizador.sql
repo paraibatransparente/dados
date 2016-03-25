e40e01d5d816c9f026c07e03a377dd0c12809a16
Diego Nobre <diego@voxtecnologia.com.br>
2016-03-25 08:25:20 -0300

    Atualizada estrutura para compatibilidade com script normalizador.py

--- tce/sagres/esfera-municipal/ddl/normalizador.sql
+++ tce/sagres/esfera-municipal/ddl/normalizador.sql
@@ -13,12 +13,12 @@ UPDATE unidade_gestora SET cd_tipo_unidade_gestora = SUBSTR(cd_ugestora, 1, 3);
 ALTER TABLE unidade_gestora ADD cd_municipio VARCHAR(3);
 UPDATE unidade_gestora SET cd_municipio = SUBSTR(cd_ugestora, 4, 6);
 
-ALTER TABLE receita_orcamentaria ADD cd_municipio VARCHAR(3);
+-- ALTER TABLE receita_orcamentaria ADD cd_municipio VARCHAR(3);
 UPDATE receita_orcamentaria SET cd_municipio = SUBSTR(cd_ugestora, 4, 6);
 
-ALTER TABLE estorno ADD cd_municipio VARCHAR(3);
+-- ALTER TABLE estorno ADD cd_municipio VARCHAR(3);
 UPDATE estorno SET cd_municipio = SUBSTR(cd_ugestora, 4, 6);
-/*
+
 ALTER TABLE empenho ADD cd_municipio VARCHAR(3);
 UPDATE empenho SET cd_municipio = SUBSTR(cd_ugestora, 4, 6);
 
@@ -27,4 +27,4 @@ UPDATE pagamento SET cd_municipio = SUBSTR(cd_ugestora, 4, 6);
 
 ALTER TABLE folha_pessoal ADD cd_municipio VARCHAR(3);
 UPDATE folha_pessoal SET cd_municipio = SUBSTR(cd_ugestora, 4, 6);
-*/
+
