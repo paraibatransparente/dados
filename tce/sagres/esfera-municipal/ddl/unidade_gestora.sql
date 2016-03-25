2ea5d86bf7154d0df56f4ff2c2f3154b68f42b93
Diego Nobre <diego@voxtecnologia.com.br>
2016-03-25 08:26:25 -0300

    Atualizada estrutura para compatibilidade com script normalizador.py

--- tce/sagres/esfera-municipal/ddl/unidade_gestora.sql
+++ tce/sagres/esfera-municipal/ddl/unidade_gestora.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS unidade_gestora;
 CREATE TABLE unidade_gestora (
   id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-  cd_ugestora INTEGER,
+  cd_ugestora VARCHAR(6),
   de_ugestora TEXT,
   ds_link TEXT
 );
