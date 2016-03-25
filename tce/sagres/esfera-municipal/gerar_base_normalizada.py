bd33b2d26427b3bd8eefed5a6600c3e9b807d796
Diego Nobre <diego@voxtecnologia.com.br>
2016-03-25 15:56:39 -0300

    Escreve comandos sqlite3 para gerar dump de tabelas de fato

--- tce/sagres/esfera-municipal/gerar_base_normalizada.py
+++ tce/sagres/esfera-municipal/gerar_base_normalizada.py
@@ -7,7 +7,8 @@ tabelas = [
     'unidade_gestora',
     'pagamento_historico_gestora_ano',
     'pagamento_historico_gestora_elemento_ano',
-    'pagamento_historico_gestora_funcao_ano'
+    'pagamento_historico_gestora_funcao_ano',
+    'receita_historico_gestora_ano',
     ]
 
 for tabela in tabelas:
