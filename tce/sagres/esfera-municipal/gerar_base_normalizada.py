fc9680166bcee28ba41ba59a35b6ee4d7af639e0
Diego Nobre <diego@voxtecnologia.com.br>
2016-04-01 07:53:49 -0300

    Atualiza geração da base padrão

--- tce/sagres/esfera-municipal/gerar_base_normalizada.py
+++ tce/sagres/esfera-municipal/gerar_base_normalizada.py
@@ -5,10 +5,11 @@ tabelas = [
     'municipio',
     'tipo_unidade_gestora',
     'unidade_gestora',
+    'receita_historico_gestora_ano',
+    'empenho_historico_gestora_ano',
     'pagamento_historico_gestora_ano',
     'pagamento_historico_gestora_elemento_ano',
     'pagamento_historico_gestora_funcao_ano',
-    'receita_historico_gestora_ano',
     ]
 
 for tabela in tabelas:
