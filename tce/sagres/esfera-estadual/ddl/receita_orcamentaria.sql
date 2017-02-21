153dc91a8b2bdbcc3a052b7bba982f0e91a58fe5
Diego Nobre <dcnobre@gmail.com>
2017-02-21 01:03:19 -0300

    Adicionados novos campos a partir da versão TCE-PB de 19/01/2017

--- tce/sagres/esfera-estadual/ddl/receita_orcamentaria.sql
+++ tce/sagres/esfera-estadual/ddl/receita_orcamentaria.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS receita_orcamentaria;
 CREATE TABLE receita_orcamentaria (
   id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  cd_Receita TEXT,
   de_Receita TEXT,
   de_AtualizacaoReceita TEXT,
   dt_MesAno VARCHAR(6),
