85c1da2e418f283fede40f2cca380fff9cac47b0
Diego Nobre <dcnobre@gmail.com>
2017-03-05 23:23:30 -0300

    Criação de índices antes de executar o SQL; adicionado 2016

--- tce/sagres/esfera-municipal/ddl/receita_historico_gestora_ano.sql
+++ tce/sagres/esfera-municipal/ddl/receita_historico_gestora_ano.sql
@@ -1,3 +1,8 @@
+CREATE INDEX IF NOT EXISTS idx_receita_orcamentaria_cd_ugestora ON receita_orcamentaria (cd_ugestora);
+CREATE INDEX IF NOT EXISTS idx_receita_orcamentaria_dt_ano ON receita_orcamentaria (dt_ano);
+CREATE INDEX IF NOT EXISTS idx_receita_orcamentaria_cd_municipio ON receita_orcamentaria (cd_municipio);
+CREATE INDEX IF NOT EXISTS idx_receita_orcamentaria_de_atualizacaoreceita ON receita_orcamentaria (de_atualizacaoreceita);
+
 DROP TABLE IF EXISTS receita_historico_gestora_ano;
 CREATE TABLE receita_historico_gestora_ano AS
 SELECT ug.cd_ugestora AS cd_ugestora
@@ -27,6 +32,11 @@ SELECT ug.cd_ugestora AS cd_ugestora
         WHERE ro.cd_UGestora = ug.cd_UGestora
           AND ro.de_atualizacaoreceita = 'Lançamento de receita'
           AND ro.dt_Ano = 2015) AS r2015
+      ,(SELECT round(sum(ro.vl_lancamentoOrc), 2)
+        FROM receita_orcamentaria ro
+        WHERE ro.cd_UGestora = ug.cd_UGestora
+          AND ro.de_atualizacaoreceita = 'Lançamento de receita'
+          AND ro.dt_Ano = 2016) AS r2016
   FROM unidade_gestora ug
--- WHERE ug.cd_tipo_unidade_gestora = '201'
+ WHERE ug.cd_municipio IN ('095', '050', '040', '171', '135', '025', '211', '046')
  GROUP BY ug.de_ugestora;
