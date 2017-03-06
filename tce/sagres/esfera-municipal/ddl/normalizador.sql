2520156180c038b7dd7ead058ef08f785a1a0751
Diego Nobre <dcnobre@gmail.com>
2017-03-05 23:22:47 -0300

    Remoção de ALTER TABLEs comentados; criação de campos de municipio

--- tce/sagres/esfera-municipal/ddl/normalizador.sql
+++ tce/sagres/esfera-municipal/ddl/normalizador.sql
@@ -1,9 +1,6 @@
--- SCRIPT NORMALIZADOR
--- CRIAÇÃO DO CAMPO id para FUNCAO
-/*
-ALTER TABLE empenho ADD funcao_id INTEGER;
-UPDATE empenho SET funcao_id = (SELECT f.id FROM funcao f WHERE f.de_Funcao = de_Funcao);
-*/
+--------------------------------------------------------------------------
+-- Criação de campos chave a partir dos dados já existentes nas tabelas --
+--------------------------------------------------------------------------
 
 -- CRIAÇÃO DO CAMPO TIPO DE UNIDADE GESTORA
 ALTER TABLE unidade_gestora ADD cd_tipo_unidade_gestora VARCHAR(3);
@@ -13,10 +10,10 @@ UPDATE unidade_gestora SET cd_tipo_unidade_gestora = SUBSTR(cd_ugestora, 1, 3);
 ALTER TABLE unidade_gestora ADD cd_municipio VARCHAR(3);
 UPDATE unidade_gestora SET cd_municipio = SUBSTR(cd_ugestora, 4, 6);
 
--- ALTER TABLE receita_orcamentaria ADD cd_municipio VARCHAR(3);
+ALTER TABLE receita_orcamentaria ADD cd_municipio VARCHAR(3);
 UPDATE receita_orcamentaria SET cd_municipio = SUBSTR(cd_ugestora, 4, 6);
 
--- ALTER TABLE estorno ADD cd_municipio VARCHAR(3);
+ALTER TABLE estorno ADD cd_municipio VARCHAR(3);
 UPDATE estorno SET cd_municipio = SUBSTR(cd_ugestora, 4, 6);
 
 ALTER TABLE empenho ADD cd_municipio VARCHAR(3);
@@ -27,4 +24,3 @@ UPDATE pagamento SET cd_municipio = SUBSTR(cd_ugestora, 4, 6);
 
 ALTER TABLE folha_pessoal ADD cd_municipio VARCHAR(3);
 UPDATE folha_pessoal SET cd_municipio = SUBSTR(cd_ugestora, 4, 6);
-
